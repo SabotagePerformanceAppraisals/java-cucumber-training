7a480543692943069f0ec6d00950267a940f0601
mjuw <mj@seattlescrum.com>
2019-12-13 20:11:03 -0800

    Let's avoid all the complications that come with floating point numbers.

--- src/main/java/com/odde/electricbartender/BeerGlass.java
+++ src/main/java/com/odde/electricbartender/BeerGlass.java
@@ -4,9 +4,9 @@ package com.odde.electricbartender;
  * We'll use the metric system to avoid confusing people who aren't used to traditional/historic units.
  */
 public class BeerGlass {
-    private static double TOTAL_CAPACITY_IN_MILLITERS = 300.0;
+    private static int TOTAL_CAPACITY_IN_MILLITERS = 300;
 
-    public double getTotalCapacityInMilliters() {
+    public int getTotalCapacityInMilliters() {
         return TOTAL_CAPACITY_IN_MILLITERS;
     }
 }
--- src/test/java/com/odde/electricbartender/BeerGlassTest.java
+++ src/test/java/com/odde/electricbartender/BeerGlassTest.java
@@ -2,7 +2,8 @@ package com.odde.electricbartender;
 
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class BeerGlassTest {
     @Test
@@ -14,7 +15,7 @@ public class BeerGlassTest {
     @Test
     public void a_beer_glass_holds_300ml() {
         BeerGlass beerGlass = new BeerGlass();
-        assertEquals(300.0, beerGlass.getTotalCapacityInMilliters());
+        assertEquals(300, beerGlass.getTotalCapacityInMilliters());
     }
 
 
