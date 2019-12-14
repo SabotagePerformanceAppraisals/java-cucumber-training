7b00f5d26a5414170a6759266fcf2a31560ca6db
mjuw <mj@seattlescrum.com>
2019-12-13 22:13:46 -0800

    get rid of boring instantiation tests, as approved by Emerson

--- src/test/java/com/odde/electricbartender/BeerGlassTest.java
+++ src/test/java/com/odde/electricbartender/BeerGlassTest.java
@@ -6,11 +6,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class BeerGlassTest {
-    @Test
-    public void a_beer_glass_can_be_created() {
-        BeerGlass beerGlass = new BeerGlass();
-        assertNotNull( beerGlass );
-    }
 
     @Test
     public void a_beer_glass_holds_300ml() {
--- src/test/java/com/odde/electricbartender/BeerKegTest.java
+++ src/test/java/com/odde/electricbartender/BeerKegTest.java
@@ -8,12 +8,6 @@ import static org.junit.Assert.assertNotNull;
 public class BeerKegTest {
 
     @Test
-    public void a_keg_can_be_created() {
-        BeerKeg beerKeg = new BeerKeg();
-        assertNotNull(beerKeg);
-    }
-
-    @Test
     public void keg_total_capacity_is_50_liters() {
         BeerKeg beerKeg = new BeerKeg();
         assertEquals(50 * 1000, beerKeg.totalCapacityInMilliliters());
